fb5ee6285d8af3931496104c8678cf893348d5eb
Johan Greus <jgreus@gmail.com>
2018-12-06 08:10:18 +0100

    Day 5 part 1

---
+++ day5/impl.fs
@@ -0,0 +1,48 @@
+module day5
+
+open System
+
+let readText filePath = System.IO.File.ReadAllText(filePath)
+
+// https://stackoverflow.com/a/20308915
+let stripchars chars str =
+  Seq.fold
+    (fun (str: string) chr ->
+      str.Replace(chr |> Char.ToUpper |> string, "").Replace(chr |> Char.ToLower |> string, ""))
+    str chars
+
+let didReact c1 c2 =
+    Char.ToUpper c1 = Char.ToUpper c2 && not(c1 = c2)
+
+let rec anyReaction chars =
+    match chars with
+    | [] -> (false, [])
+    | char::rest ->
+        match rest with
+        | [] -> (false, [])
+        | next::rest2 -> if didReact char next then (true, [char;next]) else anyReaction rest
+
+let rec react chars =
+    match anyReaction chars with
+    | false, _ -> chars
+    | true, l ->
+        (chars
+        |> List.toArray
+        |> System.String
+        ).Replace (l |> List.toArray |> System.String, "")
+        |> Seq.toList
+        |> react
+
+let test () =
+    "dabAcCaCBAcCcaDA"
+    |> Seq.toList
+    |> react
+    |> List.toArray |> System.String
+
+
+let p1 () =
+    readText "input"
+    |> stripchars ['\n']
+    |> Seq.toList
+    |> react
+    |> List.length
